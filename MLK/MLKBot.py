8a69c3c4f277fc7dcb2403ce0d34e61a9e1b542a
JamesRobEaston <james.easton@centre.edu>
2020-01-24 11:46:44 -0500

    Added functionality for blinking lights and turning the button LED on. Made it so that there is a backup text-to-speech library in use.

--- MLK/MLKBot.py
+++ MLK/MLKBot.py
@@ -2,16 +2,20 @@
 import speech_recognition as sr     # import the library
 import RPi.GPIO as GPIO
 import subprocess
+from multiprocessing import Process
 import time
 import os
 import random
 from Tkinter import *
 import tkSnack
 from Dataset import Dataset
+from gtts import gTTS
 #from Real-Time-Voice-Cloning import VoiceCloning.Toolbox
 
 random.seed()
-audioProcess = None    
+audioProcess = None   
+processing_led_1 = 21 
+processing_led_2 = 16
 
 def getResponseObjects(responses, dataset):
     responseObjects = []
@@ -21,7 +25,20 @@ def getResponseObjects(responses, dataset):
     return responseObjects
 
 def calculateResponseObjectFitness(responseObject, responseFrequency):
-    return float(responseFrequency)/len(responseObject["keys"]) 
+    return float(responseFrequency)/len(responseObject["keys"])
+
+def flashLights():
+    pauseTime = 1
+    while True:
+        GPIO.output(processing_led_1, True)
+        GPIO.output(processing_led_2, True)
+        time.sleep(pauseTime)
+        GPIO.output(processing_led_1, False)
+        GPIO.output(processing_led_2, False)
+        time.sleep(pauseTime)
+    
+def stopLights():
+    GPIO.output(processing_led_1, GPIO.LOW)
 
 def getFinalResponse(text, dataset):
     requiredMaxFrequency = 3
@@ -59,7 +76,7 @@ def getFinalResponse(text, dataset):
             highestFitnessIndex = i
     
     #Return the best response
-    finalResponse = []
+    finalResponse = {}
     finalResponse["fact"] = "I don't know how to respond to that"
     finalResponse["fact_type"] = "t"
     if highestFitnessIndex != -1 and (maxFrequency > requiredMaxFrequency or responsesFitness[highestFitnessIndex] > requiredAccuracy):
@@ -71,7 +88,10 @@ def playAudio(output):
     audioProcess = subprocess.Popen(['omxplayer', '-o', 'alsa', output], stdin=subprocess.PIPE)
 
 def speak(output):
-    print(output)
+    tts = gTTS(text=output, lang='en')
+    print('Saving...')
+    tts.save("speech.mp3")
+    os.system("mpg321 speech.mp3")
 
 def main():
     dataset = Dataset()
@@ -80,15 +100,26 @@ def main():
     GPIO.setwarnings(False) # Avoids warning channel is already in use
 
     button = 18 # GPIO pin 18
+    button_led = 17
 
     GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP) #sets up pin 18 as a button
+    GPIO.setup(button_led, GPIO.OUT)
+    GPIO.setup(processing_led_1, GPIO.OUT)
+    GPIO.setup(processing_led_2, GPIO.OUT)
+    GPIO.output(button_led, True)
 
     i_count = 0 # set up for correct grammar in notification below
 
     r = sr.Recognizer()
-    while True:
+    cont = True
+    while cont:
         input_state = GPIO.input(button) # primes the button!
+        
         if input_state == False:
+            
+            flashLightsProcess = Process(target=flashLights)
+            flashLightsProcess.start()
+            
             if audioProcess:
                 audioProcess.stdin.write('q')
             with sr.Microphone() as source:     # mention source it will be either Microphone or audio files.
@@ -100,18 +131,26 @@ def main():
                 try:
                     text = r.recognize_google(audio)    # use recognizer to convert our audio into text part.
                     print("You said : {}".format(text))
-                    finalResponse = getFinalResponse(text, dataset)
-                    output = finalResponse["fact"]
-                    outputType = finalResponse["fact_type"]
+                    if text != 'stop':
+                        finalResponse = getFinalResponse(text, dataset)
+                        output = finalResponse["fact"]
+                        outputType = finalResponse["fact_type"]
+                    else:
+                        cont = False
                 except Exception as e:
                     print(e)
-                if outputType == 'a':
-                    playAudio(dataset.getFilePath(output))
-                elif outputType == 'r':
-                    choice = output[random.randint(0, len(output) - 1)]
-                    speak(choice)
-                else:
-                    speak(output)
+                if cont:
+                    if outputType == 'a':
+                        playAudio(dataset.getFilePath(output))
+                    elif outputType == 'r':
+                        choice = output[random.randint(0, len(output) - 1)]
+                        speak(choice)
+                    else:
+                        speak(output)
+            
+            flashLightsProcess.terminate()
+            stopLights()
             time.sleep(1.0)
+    GPIO.cleanup()
 
 main()
