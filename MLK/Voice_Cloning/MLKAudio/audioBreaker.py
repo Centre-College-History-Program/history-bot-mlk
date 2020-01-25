49a1dfb478c0ad10f098cb1bfed184d48689c816
JamesRobEaston <james.easton@centre.edu>
2020-01-25 10:38:10 -0500

    Removed incompatible audio files.

--- MLK/Voice_Cloning/MLKAudio/audioBreaker.py
+++
@@ -1,5 +0,0 @@
-import glob, os
-for file in sorted(glob.glob("*.flac")):
-    print("#"*40)
-    print(file)
-    os.system("python audioAnalysis.py silenceRemoval -i " + file + " --smoothing 0.2 --weight 0.1")
